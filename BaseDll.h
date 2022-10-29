a7f5f7af528cd5a355e37f6fe0088ff56a88a115
vxiiduu <73044267+vxiiduu@users.noreply.github.com>
2022-10-29 10:31:10 +1000

    Delete BaseDll.h

--- BaseDll.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <NtDll.h>
-
-// This routine converts a NTSTATUS to a Win32 error code and (effectively) SetLastError()'s it.
-INLINE DWORD BaseSetLastNTError(
-	IN	NTSTATUS	st)
-{
-	return (NtCurrentTeb()->LastErrorValue = RtlNtStatusToDosError(st));
-}
