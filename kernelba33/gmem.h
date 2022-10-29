d41a917f4c5166b6d7dd5874c3425ab3a1a58dbe
vxiiduu <73044267+vxiiduu@users.noreply.github.com>
2022-10-29 10:30:53 +1000

    Delete kernelba33 directory

--- kernelba33/gmem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include <KexComm.h>
-#include <KexDll.h>
-
-typedef struct _WIN32_MEMORY_RANGE_ENTRY {
-	LPVOID	VirtualAddress;
-	SIZE_T	NumberOfBytes;
-} WIN32_MEMORY_RANGE_ENTRY, *PWIN32_MEMORY_RANGE_ENTRY;
